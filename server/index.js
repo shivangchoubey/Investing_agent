6a66b9db369c4631ac1bb08123da1a299e937235
ShivangChoubey <shivang.c2004@gmail.com>
2026-07-12 23:14:51 +0530

    Prepare backend for deployment

--- server/index.js
+++ server/index.js
@@ -13,8 +13,8 @@ app.use('/api',aiRoutes);
 app.get('/',(req,res)=>{
     res.send("Backend is Running");
 })
-const port=5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`)
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`)
 });
